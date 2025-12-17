ebc58b8e2fb78618cd2e027ee1a28774055f6e15
Armando233-abc <armandovigliotti5@gmail.com>
2025-12-17 19:00:05 +0100

    scheletro micro-servizio

---
+++ Scenario/src/main/java/com/tirocinio/Scenario/ScenarioApplication.java
@@ -0,0 +1,13 @@
+package com.tirocinio.Scenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenarioApplication.class, args);
+	}
+
+}
---
+++ Scenario/src/test/java/com/tirocinio/Scenario/ScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirocinio.Scenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
