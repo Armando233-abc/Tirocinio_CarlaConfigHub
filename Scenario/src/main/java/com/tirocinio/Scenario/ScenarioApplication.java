b727e4191b6e477175b65924766bff61682022db
Armando233-abc <armandovigliotti5@gmail.com>
2025-12-17 19:02:18 +0100

    piccole modifiche

--- Scenario/src/main/java/com/tirocinio/Scenario/ScenarioApplication.java
+++ Scenario/src/main/java/com/tirocinio/Scenario/ScenarioApplication.java
@@ -3,6 +3,7 @@ package com.tirocinio.Scenario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ScenarioApplication {
