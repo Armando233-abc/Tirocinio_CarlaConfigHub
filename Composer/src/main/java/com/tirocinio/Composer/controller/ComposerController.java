51e51b9c3abab8ce641a24643766c065407d1a8a
Armando233-abc <armandovigliotti5@gmail.com>
2025-12-22 11:21:08 +0100

    implementazione del microservizio Composer

--- Composer/src/main/java/com/tirocinio/Composer/controller/ComposerController.java
+++ Composer/src/main/java/com/tirocinio/Composer/controller/ComposerController.java
@@ -1,4 +1,36 @@
 package com.tirocinio.Composer.controller;
 
+
+import com.tirocinio.Composer.service.ComposerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/composer")
 public class ComposerController {
+
+    private final ComposerService composerService;
+
+    @Autowired
+    public ComposerController(ComposerService composerService) {
+        this.composerService = composerService;
+    }
+
+    @PostMapping("/compose")
+    public ResponseEntity<?> compose(@RequestBody Map<String, String> fragments) {
+        try {
+            System.out.println("ComposerService (Port 8085): Ricevuti frammenti da assemblare.");
+
+            String finalXml = composerService.composeConfiguration(fragments);
+
+            return ResponseEntity.ok(finalXml);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Errore Composer: " + e.getMessage());
+        }
+    }
 }
--- Composer/src/main/java/com/tirocinio/Composer/service/ComposerService.java
+++ Composer/src/main/java/com/tirocinio/Composer/service/ComposerService.java
@@ -1,4 +1,58 @@
 package com.tirocinio.Composer.service;
 
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import java.util.Map;
+
+@Service
 public class ComposerService {
+
+    public String composeConfiguration(Map<String, String> fragments) {
+
+        String weatherFragment = fragments.getOrDefault("weatherFragment", "");
+        String vehicleFragment = fragments.getOrDefault("vehicleFragment", "");
+        String scenarioFragment = fragments.getOrDefault("scenarioFragment", "");
+
+
+        StringBuilder finalXml = new StringBuilder();
+        finalXml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        finalXml.append("<CarlaConfig>\n");
+
+
+        finalXml.append(weatherFragment).append("\n");
+        finalXml.append(vehicleFragment).append("\n");
+        finalXml.append(scenarioFragment).append("\n");
+
+        finalXml.append("</CarlaConfig>");
+
+        String result = finalXml.toString();
+
+
+        if (!isValidXml(result)) {
+            throw new RuntimeException("L'XML generato non è valido o è corrotto.");
+        }
+
+        return result;
+    }
+
+
+    private boolean isValidXml(String xmlString) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse(new InputSource(new StringReader(xmlString)));
+            return true;
+        } catch (Exception e) {
+            System.err.println("Validazione XML fallita: " + e.getMessage());
+            return false;
+        }
+    }
 }
