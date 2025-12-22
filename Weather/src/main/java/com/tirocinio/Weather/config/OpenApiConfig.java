3f0c3f460656278fd723dfbf190571c99cddfa47
Armando233-abc <armandovigliotti5@gmail.com>
2025-12-22 12:15:56 +0100

    Integrazione Swagger/OpenAPI nel Weather Service con sicurezza profili
    - Aggiunta la dipendenza SpringDoc nel pom.xml
    - Creata classe OpenApiConfig specifica per il servizio Meteo
    - Impostato Swagger come disabilitato di default (produzione)
    - Abilitato Swagger solo nel profilo 'dev' (application-dev.properties)

---
+++ Weather/src/main/java/com/tirocinio/Weather/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.tirocinio.Weather.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI weatherServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Weather Service API")
+                        .description("Microservizio per la gestione e validazione dei parametri meteorologici (Nuvole, Pioggia, Vento, Sole).")
+                        .version("1.0.0"));
+    }
+}
