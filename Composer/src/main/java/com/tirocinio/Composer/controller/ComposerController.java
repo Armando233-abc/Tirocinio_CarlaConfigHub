21b2af4af1c70c6205ccd3f6b6b05b316df1f09a
Armando233-abc <armandovigliotti5@gmail.com>
2025-12-22 18:07:41 +0100

    aggiunta notazioni swagger all'interno del ComposerController

--- Composer/src/main/java/com/tirocinio/Composer/controller/ComposerController.java
+++ Composer/src/main/java/com/tirocinio/Composer/controller/ComposerController.java
@@ -2,6 +2,9 @@ package com.tirocinio.Composer.controller;
 
 
 import com.tirocinio.Composer.service.ComposerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,15 @@ public class ComposerController {
         this.composerService = composerService;
     }
 
+    @Operation(
+            summary = "Assemblaggio e Validazione Finale",
+            description = "Aggrega i frammenti XML ricevuti dal gateway in un unico documento e verifica la conformità sintattica e strutturale."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File di configurazione CARLA completo e validato"),
+            @ApiResponse(responseCode = "422", description = "Errore di validazione: l'XML risultante è sintatticamente errato o incompleto."),
+            @ApiResponse(responseCode = "500", description = "Errore durante il processo di assemblaggio")
+    })
     @PostMapping("/compose")
     public ResponseEntity<?> compose(@RequestBody Map<String, String> fragments) {
         try {
